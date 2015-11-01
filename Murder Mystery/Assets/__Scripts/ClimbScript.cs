c75bf20009dd4a155b33454486807c002a865772
Mohammed Joy <moejo@umich.edu>
2015-11-01 15:57:40 -0500

    First Commit. Really really simple. I made the stairs a singleton, that definitely has to change. Its just basic layout and movement. You Right and Left arrow keys to move. You can climb up and down once you trigger the stairs singleton, but that has to change. I made it really quickly last night. Didn't really think about it too much.

---
+++ Murder Mystery/Assets/__Scripts/ClimbScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClimbScript : MonoBehaviour {
+	static public ClimbScript S;
+	public bool canMove = false;
+	// Use this for initialization
+	void Start () {
+		S = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		print ("Triggered bitches");
+		canMove = true;
+	}
+
+	void OnTriggerExit(Collider other){
+		canMove = false;
+	}
+}
---
+++ Murder Mystery/Assets/__Scripts/Movement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (!ClimbScript.S.canMove) {
+			gameObject.GetComponent<Rigidbody>().useGravity = true;
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate(Vector3.right*Time.deltaTime*speed);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate(Vector3.left*Time.deltaTime*speed);
+		}
+
+		if (ClimbScript.S.canMove) {
+			gameObject.GetComponent<Rigidbody>().useGravity = false;
+
+			if (Input.GetKey (KeyCode.UpArrow)) {
+				transform.Translate (Vector3.up * Time.deltaTime * speed);
+			}
+			if (Input.GetKey (KeyCode.DownArrow)) {
+				transform.Translate (Vector3.down * Time.deltaTime * speed);
+			}
+		}
+
+
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.name == "Stairs") {
+			print("Stairs");
+
+			if (Input.GetKey (KeyCode.UpArrow)) {
+				transform.Translate(Vector3.up*Time.deltaTime*speed);
+			}
+			if (Input.GetKey (KeyCode.DownArrow)) {
+				transform.Translate(Vector3.down*Time.deltaTime*speed);
+			}
+		}
+
+	}
+}
