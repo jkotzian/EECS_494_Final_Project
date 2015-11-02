e57246f778bf64d1814b58ee722451d59f3a3395
Daniel Rings <rings.daniel@gmail.com>
2015-11-01 23:09:45 -0500

    Detective/Murderer preliminary interaction

---
+++ Murder Mystery/Assets/__Scripts/Detective.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Detective : MonoBehaviour {
+    public GameObject detective;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        //Make Arrest
+        if (Input.GetKey(KeyCode.RightShift))
+        {
+            GameObject murderer = GameObject.Find("Player1");
+            Vector3 detectivePos = gameObject.transform.position;
+            Vector3 murdererPos = murderer.transform.position;
+            if((detectivePos - murdererPos).magnitude < 1)
+            {
+                //Knock the murderer over
+                murderer.transform.Rotate(new Vector3(0, 0, 90));
+            }
+        }
+    }
+}
