5670ce79983a19bac7ed17c1f83cc4d1875b94e2
Daniel Rings <rings.daniel@gmail.com>
2015-11-22 02:00:45 -0500

    added victory scenes

---
+++ Murder Mystery/Assets/__Scripts/Restart.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Restart : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Application.LoadLevel("AlphaMansion");
+        }
+	}
+}
