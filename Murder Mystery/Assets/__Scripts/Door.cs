611234c4c5b710a090ec3cdb378902852cae1dd4
DanielRings <rings.daniel@gmail.com>
2015-11-22 13:37:10 -0500

    elevator logic is cleaner

--- Murder Mystery/Assets/__Scripts/Door.cs
+++ Murder Mystery/Assets/__Scripts/Door.cs
@@ -3,31 +3,24 @@ using System.Collections;
 
 public class Door : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public Door above;
+    public Door below;
 
 	void OnTriggerStay(Collider other){
+        Movement movement = other.GetComponent<Movement>();
 		//sending the characters up and down a level 
-		if(other.GetComponent<Movement>() != null){
-			if (other.GetComponent<Movement> ().isDetective == true && Input.GetKeyDown(other.GetComponent<Movement> ().upKey) && gameObject.name != "TopDoor") {
-				print ("Triggered");
-				Vector3 temp = other.transform.position;
-				temp.y += 2.5f;
-				other.transform.position = temp;
-			}
-
-			if (other.GetComponent<Movement> ().isDetective == true && Input.GetKeyDown(other.GetComponent<Movement> ().downKey) && gameObject.name != "BottomDoor") {
-				print ("Triggered");
-				Vector3 temp = other.transform.position;
-				temp.y -= 2.5f;
-				other.transform.position = temp;
+		if(movement && movement.isDetective){
+            // move up
+			if (Input.GetKeyDown(movement.upKey) && above) {
+				print ("up elevator");
+                Vector3 dest = new Vector3(above.transform.position.x, above.transform.position.y, -0.2f);
+                other.transform.position = dest;
+            }
+            // move down
+			if (Input.GetKeyDown(movement.downKey) && below) {
+				print ("down elevator");
+                Vector3 dest = new Vector3(below.transform.position.x, below.transform.position.y, -0.2f);
+                other.transform.position = dest;
 			}
 		}
 	}
