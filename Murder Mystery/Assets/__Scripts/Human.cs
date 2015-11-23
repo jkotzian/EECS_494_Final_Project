414d56bc2e8fc91486e97e16c0c7ff720e7c1562
jeffmorr <jeffmorr@umich.edu>
2015-11-23 00:57:37 -0500

    Added removal of ghost from environmental kill
    When you kill an NPC, the ghost is removed.

--- Murder Mystery/Assets/__Scripts/Human.cs
+++ Murder Mystery/Assets/__Scripts/Human.cs
@@ -39,6 +39,7 @@ public class Human : MonoBehaviour {
     public void Kill()
     {
         //Knock the murderer over
+		GetComponent<NPC> ().dispossess ();
         transform.Rotate(new Vector3(0, 0, 90));
         alive = false;
         TotalGame.S.bodyCount[TotalGame.S.round - 1]++;
--- Murder Mystery/Assets/__Scripts/Switch.cs
+++ Murder Mystery/Assets/__Scripts/Switch.cs
@@ -32,46 +32,50 @@ public class Switch : MonoBehaviour {
 	}
 
 	void OnTriggerStay(Collider other){
-		Movement movement = other.GetComponent<Movement> ();
-		bool pressed = Input.GetKeyDown (KeyCode.E);
-		if (movement && pressed) {
-			print ("Hey");
-			print (switchNum);
-			if (switchNum == 1){	
-				rigidbody2Dimensional = GamePlay.S.EnvironmentalObjects [0].GetComponent<Rigidbody> ();
-                rigidbody2Dimensional.useGravity = true;
-			}
-			else if (switchNum == 2){
-				other.GetComponent<BoxCollider>().enabled = false;
-			}
-			else if (switchNum == 3){
-				Color tmpColor = GamePlay.S.EnvironmentalObjects [1].GetComponent<Renderer> ().material.color;
-				tmpColor.a = 0.5f;
-				GamePlay.S.EnvironmentalObjects [1].GetComponent<Renderer> ().material.color = tmpColor;
-				Vector3 tmpPos = GamePlay.S.EnvironmentalObjects [1].GetComponent<Transform>().position;
-				tmpPos.z = 0;
-				GamePlay.S.EnvironmentalObjects [1].GetComponent<Transform>().position = tmpPos;
-				other.gameObject.GetComponent<Human> ().Kill ();
-			}
-			else if (switchNum == 4){
-				GamePlay.S.EnvironmentalObjects[2].GetComponent<Flamethrower>().flameOn = true;
-			}
-			else if (switchNum == 5){
-				this.GetComponent<Renderer>().material = emptyMaterial;
-				murderer = other.gameObject;
-				isInfected = true;
-			}
-			else if (switchNum == 6 && !isChopped){
-				GamePlay.S.EnvironmentalObjects[3].GetComponent<Transform>().Rotate(new Vector3(0, 0, 90));
-				other.gameObject.GetComponent<Human> ().Kill ();
-				isChopped = true;
-			}
-			else if (switchNum == 7 && !droppedLid){
-				print ("Got in");
-				GamePlay.S.EnvironmentalObjects[4].GetComponent<Transform>().Rotate(new Vector3(0, 0, 320));
-				this.transform.Rotate(new Vector3(0, 0, -60));
-				other.gameObject.GetComponent<Human> ().Kill ();
-				droppedLid = true;
+		NPC computer = other.GetComponent<NPC> ();
+		bool pressed = Input.GetKeyDown (KeyCode.Q);
+		if (computer != null) {
+			if (computer.possessed && pressed) {
+				print ("Hey");
+				print (switchNum);
+				if (switchNum == 1) {
+					GamePlay.S.EnvironmentalObjects [0].GetComponent<BoxCollider>().enabled = true;
+					rigidbody2Dimensional = GamePlay.S.EnvironmentalObjects [0].GetComponent<Rigidbody> ();
+					rigidbody2Dimensional.useGravity = true;
+					//computer.dispossess();
+				} else if (switchNum == 2) {
+					other.GetComponent<BoxCollider> ().enabled = false;
+					computer.dispossess();
+				} else if (switchNum == 3) {
+					Color tmpColor = GamePlay.S.EnvironmentalObjects [1].GetComponent<Renderer> ().material.color;
+					tmpColor.a = 0.5f;
+					GamePlay.S.EnvironmentalObjects [1].GetComponent<Renderer> ().material.color = tmpColor;
+					Vector3 tmpPos = GamePlay.S.EnvironmentalObjects [1].GetComponent<Transform> ().position;
+					tmpPos.z = 0;
+					GamePlay.S.EnvironmentalObjects [1].GetComponent<Transform> ().position = tmpPos;
+					other.gameObject.GetComponent<Human> ().Kill ();
+					//computer.dispossess();
+				} else if (switchNum == 4) {
+					GamePlay.S.EnvironmentalObjects [2].GetComponent<Flamethrower> ().flameOn = true;
+					//computer.dispossess();
+				} else if (switchNum == 5) {
+					this.GetComponent<Renderer> ().material = emptyMaterial;
+					murderer = other.gameObject;
+					isInfected = true;
+					//computer.dispossess();
+				} else if (switchNum == 6 && !isChopped) {
+					GamePlay.S.EnvironmentalObjects [3].GetComponent<Transform> ().Rotate (new Vector3 (0, 0, 90));
+					other.gameObject.GetComponent<Human> ().Kill ();
+					isChopped = true;
+					//computer.dispossess();
+				} else if (switchNum == 7 && !droppedLid) {
+					print ("Got in");
+					GamePlay.S.EnvironmentalObjects [4].GetComponent<Transform> ().Rotate (new Vector3 (0, 0, 320));
+					this.transform.Rotate (new Vector3 (0, 0, -60));
+					other.gameObject.GetComponent<Human> ().Kill ();
+					droppedLid = true;
+					//computer.dispossess();
+				}
 			}
 		}
 	}
