64070b982337dea857428c7da74e50174f61571f
Mohammed Joy <moejo@umich.edu>
2015-12-12 15:49:03 -0500

    Easing the lights delay at the end of the game, so just a black screen doesn't pop up. it eases into darkness.

---
+++ Murder Mystery/Assets/__Scripts/EaseLightsOff.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EaseLightsOff : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GamePlay.S.gameOver) {
+			gameObject.GetComponent<Light>().intensity -= gameObject.GetComponent<Light>().intensity*.015f;
+		}
+	}
+}
