fa5f63e5130cf5514283ee368dfa40880ebc29b4
James Kotzian <jkotzian@umich.edu>
2015-12-18 01:51:42 -0500

    Added credits screen and changed analog select logic

--- Murder Mystery/Assets/__Scripts/TotalGame.cs
+++ Murder Mystery/Assets/__Scripts/TotalGame.cs
@@ -10,6 +10,8 @@ public class TotalGame : MonoBehaviour {
     public GameObject NGGhost;
     public GameObject TDetective;
     public GameObject TGhost;
+    public GameObject CDetective;
+    public GameObject CGhost;
 
     public int round;
     public List<int> bodyCount;
@@ -17,6 +19,9 @@ public class TotalGame : MonoBehaviour {
 
     public bool inMainMenu;
     public bool inReady;
+    public bool inCredits;
+
+    bool routineRunning;
 
     void Awake()
     {
@@ -31,7 +36,10 @@ public class TotalGame : MonoBehaviour {
         NGGhost.SetActive(true);
         TDetective.SetActive(false);
         TGhost.SetActive(false);
+        CDetective.SetActive(false);
+        CGhost.SetActive(false);
         DontDestroyOnLoad(transform.gameObject);
+        routineRunning = false;
     }
 
     // Update is called once per frame
@@ -41,32 +49,37 @@ public class TotalGame : MonoBehaviour {
             bool actionPressed = false;
             bool upPressed = false;
             bool downPressed = false;
+            bool analogUpPressed = false;
+            bool analogDownPressed = false;
             float analogAngleLeeway = .3f;
             float analogThrustThreshold = .9f;
             for (int i = 0; i < numControllers; i++)
             {
                 InputDevice controller = InputManager.Devices[i];
-                bool up = ((controller.LeftStick.Vector.x > -analogAngleLeeway &&
+                bool analogUp = ((controller.LeftStick.Vector.x > -analogAngleLeeway &&
                    controller.LeftStick.Vector.x < analogAngleLeeway &&
                    controller.LeftStick.Vector.y > 0 &&
-                   controller.LeftStickY > analogThrustThreshold) ||
-                   controller.DPadUp);
-                bool down = ((controller.LeftStick.Vector.x > -analogAngleLeeway &&
+                   controller.LeftStickY > analogThrustThreshold));
+
+                bool analogDown = ((controller.LeftStick.Vector.x > -analogAngleLeeway &&
                           controller.LeftStick.Vector.x < analogAngleLeeway &&
                           controller.LeftStick.Vector.y < 0 &&
-                          controller.LeftStickY < -analogThrustThreshold) ||
-                          controller.DPadDown);
+                          controller.LeftStickY < -analogThrustThreshold));
+
+                upPressed = controller.DPadUp.WasPressed;
+                downPressed = controller.DPadDown.WasPressed;
+
                 if (controller.Action1.WasPressed)
                 {
                     actionPressed = true;
                 }
-                if (up)
+                if (analogUp)
                 {
-                    upPressed = true;
+                    analogUpPressed = true;
                 }
-                if (down)
+                if (analogDown)
                 {
-                    downPressed = true;
+                    analogDownPressed = true;
                 }
             }
             // Keyboard selection
@@ -78,21 +91,13 @@ public class TotalGame : MonoBehaviour {
             {
                 downPressed = true;
             }
-            if (upPressed && selected == 1)
+            // If there's not a directly pressed key or dpad, but the analog is used,
+            // do the delay select
+            if (!routineRunning && !upPressed && !downPressed && (analogUpPressed || analogDownPressed))
+                StartCoroutine(checkForSelectionChange(analogUpPressed, analogDownPressed));
+            if (upPressed || downPressed)
             {
-                NGDetective.SetActive(true);
-                NGGhost.SetActive(true);
-                TDetective.SetActive(false);
-                TGhost.SetActive(false);
-                selected = 0;
-            }
-            if (downPressed && selected == 0)
-            {
-                TDetective.SetActive(true);
-                TGhost.SetActive(true);
-                NGDetective.SetActive(false);
-                NGGhost.SetActive(false);
-                selected = 1;
+                checkForSelectionChangeHelper(upPressed, downPressed);
             }
             if (Input.GetKeyDown(KeyCode.Space) || actionPressed)
             {
@@ -106,7 +111,87 @@ public class TotalGame : MonoBehaviour {
                 {
                     StartCoroutine(startTutorial());
                 }
+                // Credits
+                else if (selected == 2)
+                {
+                    StartCoroutine(startCredits());
+                }
             }
+        }
+        if (inCredits)
+        {
+            bool actionPressed = false; 
+
+            int numControllers = InputManager.Devices.Count;
+
+            for (int i = 0; i < numControllers; i++)
+            {
+                InputDevice controller = InputManager.Devices[i];
+                if (controller.Action1.WasPressed)
+                {
+                    actionPressed = true;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                actionPressed = true;
+            }
+
+            if (actionPressed)
+            {
+                Application.LoadLevel("MainMenu");
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    IEnumerator checkForSelectionChange(bool upPressed, bool downPressed)
+    {
+        routineRunning = true;
+        yield return new WaitForSeconds(.15f);
+        checkForSelectionChangeHelper(upPressed, downPressed);
+        routineRunning = false;
+    }
+
+    void checkForSelectionChangeHelper(bool upPressed, bool downPressed)
+    {
+        //print("here");
+        // Tutorial to New Game
+        if (upPressed && selected == 1)
+        {
+            NGDetective.SetActive(true);
+            NGGhost.SetActive(true);
+            TDetective.SetActive(false);
+            TGhost.SetActive(false);
+            selected = 0;
+        }
+        // Credits to Tutorial
+        else if (upPressed && selected == 2)
+        {
+            CDetective.SetActive(false);
+            CGhost.SetActive(false);
+            TDetective.SetActive(true);
+            TGhost.SetActive(true);
+            selected = 1;
+        }
+        // New Game to Tutorial
+        if (downPressed && selected == 0)
+        {
+            TDetective.SetActive(true);
+            TGhost.SetActive(true);
+            NGDetective.SetActive(false);
+            NGGhost.SetActive(false);
+            selected = 1;
+        }
+        // Tutorial to New Game
+        else if (downPressed && selected == 1)
+        {
+            CDetective.SetActive(true);
+            CGhost.SetActive(true);
+            TDetective.SetActive(false);
+            TGhost.SetActive(false);
+            selected = 2;
         }
     }
 
@@ -130,5 +215,17 @@ public class TotalGame : MonoBehaviour {
         inReady = true;
         inMainMenu = false;
         Application.LoadLevel("Tutorial");
+    }
+
+    IEnumerator startCredits()
+    {
+        AudioSource weaponFireSound = CDetective.GetComponent<Detective>().weaponFireSoundObj.GetComponent<AudioSource>();
+        weaponFireSound.Play();
+        CDetective.GetComponent<Detective>().weaponEffect.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        inReady = false;
+        inMainMenu = false;
+        inCredits = true;
+        Application.LoadLevel("Credits");
     }
 }
